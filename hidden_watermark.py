f0efe9bd8588486f5edd1ee597ff2454bf23ab74
CHERIF2001 <cherifaziz319@gmail.com>
2024-01-09 13:59:18 +0100

    i create a new function lsb1_extract message

--- hidden_watermark.py
+++ hidden_watermark.py
@@ -30,13 +30,48 @@ def lsb1_stegano(image_path, message):
 
 
 
+
 	#print(image_array[:,:,:])
 
-	cv2.imwrite("C:/HETIC/python/ala.png", image_array)
+	cv2.imwrite("../watermarked_image.png", image_array)
 	cv2.imshow("aladdin", image_array)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
 
+def lsb1_extract_message(watermarked_image_path):
+	watermarked_image_array = cv2.imread(watermarked_image_path)
+	binary_array_message = watermarked_image_array % 2
+
+
+# On flatten la matrice binaire qui contient le message caché
+	nb_rows, nb_cols, nb_canals= watermarked_image_array.shape
+	binary_message_list = []
+
+	for index_row in range(nb_rows):
+		for index_col in range(nb_cols):
+			for index_canal in range(nb_canals):				
+				binary_message_list.append(str(binary_array_message[index_row, index_col, index_canal]))
+
+
+	"""print(binary_message_list)
+	print("_"*80)"""
+# On élimine les caractères vides 
+	for index_binary_char in range(0, nb_rows*nb_cols*nb_canals,8):
+		if binary_message_list[index_binary_char: index_binary_char+8] ==["0"]*8:
+			binary_message_list = binary_message_list[: index_binary_char]  #le slicing
+			break
+	#print(binary_message_list)
+
+# On convertit le message binaire pour recupérer le message caché 
+	binary_message = "".join(binary_message_list)
+	message = "".join([chr(int(binary_message[i:i+ 8], 2)) for i in range(0, len(binary_message),8)])
+
+	print(message)
+
+
+
+
 if __name__=="__main__":
 	lsb1_stegano("../aladdin.jpg","Hello les amis")
+	lsb1_extract_message("../watermarked_image.png")
